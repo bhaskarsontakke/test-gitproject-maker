6f4187037b2a67a37c61a25142375cdc246d650f
seeded-enterprise-maker <bhaskar.sontakke@smartbear.com>
2024-10-15 11:37:58 +0530

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight-local.com:8443/api/v1/projects/cHJqOjEwNzc/spectral.js?branch_slug=main&token=de32635b-cdf4-42fb-be51-e1bc68d043a1";
+export default { extends: ruleset };
